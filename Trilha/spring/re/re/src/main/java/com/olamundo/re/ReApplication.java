9483452c7295c9f417d74b8375037d2745151c32
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-02-28 21:29:26 -0300

    Add files via upload

---
+++ Trilha/spring/re/re/src/main/java/com/olamundo/re/ReApplication.java
@@ -0,0 +1,13 @@
+package com.olamundo.re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReApplication.class, args);
+	}
+
+}
---
+++ Trilha/spring/re/re/src/test/java/com/olamundo/re/ReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olamundo.re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
