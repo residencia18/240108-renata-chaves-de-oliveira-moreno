a8c7175669410370260b9d030dd67ca9b8cff7bd
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-02-29 23:50:47 -0300

    Update Usuario.java
    ajuste

--- PROVA/PO_14/leiloajava/leiloajava/src/main/java/com/leilaoonline/leiloajava/modelo/Usuario.java
+++ PROVA/PO_14/leiloajava/leiloajava/src/main/java/com/leilaoonline/leiloajava/modelo/Usuario.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-
 @Entity
 public class Usuario {
     @Id
@@ -16,21 +15,51 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
-	public List<Lance> getListaLances() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public Long getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public String getNome() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public String getCpf() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
+    // Métodos getters e setters para os atributos
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    // Método para retornar a lista de lances associada ao usuário
+    public List<Lance> getListaLances() {
+        // Implemente este método para retornar a lista de lances associada ao usuário
+        return null;
+    }
+
+    // Método para retornar o CPF do usuário (caso esteja disponível)
+    public String getCpf() {
+        // Implemente este método para retornar o CPF do usuário, se estiver disponível
+        return null;
+    }
 }
+
