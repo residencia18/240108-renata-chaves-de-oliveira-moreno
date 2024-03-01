a03bb939af1d39034a2736657529d6e17c49da44
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-01 00:19:46 -0300

    Update LeilaoDTO.java

--- PROVA/PO_14/leiloajava/leiloajava/src/main/java/com/leilaoonline/leiloajava/controller/dto/LeilaoDTO.java
+++ PROVA/PO_14/leiloajava/leiloajava/src/main/java/com/leilaoonline/leiloajava/controller/dto/LeilaoDTO.java
@@ -4,18 +4,34 @@ import com.leilaoonline.leiloajava.modelo.Leilao;
 
 public class LeilaoDTO {
     private Long id;
-    private String nome;
     private String descricao;
 
     // Construtor
-    public LeilaoDTO(Long id, String nome, String descricao) {
+    public LeilaoDTO(Long id, String descricao) {
         this.id = id;
-        this.nome = nome;
         this.descricao = descricao;
     }
 
-	public LeilaoDTO(Leilao leilao) {
-		// TODO Auto-generated constructor stub
-	}
+    // Construtor para converter Leilao em LeilaoDTO
+    public LeilaoDTO(Leilao leilao) {
+        this.id = leilao.getId();
+        this.descricao = leilao.getDescricao();
+    }
 
+    // Getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
