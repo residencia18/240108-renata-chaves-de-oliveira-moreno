de10ec1bc9b645d63d10d10bd1a1720d841899f9
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-07 00:54:16 -0300

    Add files via upload

---
+++ FEB-P014/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// Modulos
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+// Componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
+import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegistrarUsuarioComponent,
+    VerificarCorreoComponent,
+    RecuperarPasswordComponent,
+    SpinnerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FEB-P014/src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCpUPNMmXCU62aBi4KlQnzy7dRpmkxeN0I",
+    authDomain: "febp014.firebaseapp.com",
+    projectId: "febp014",
+    storageBucket: "febp014.appspot.com",
+    messagingSenderId: "187642877275",
+    appId: "1:187642877275:web:9845ee06130feac2b8f67e",
+    measurementId: "G-JVVE5DLSK4"
+  }
+};
