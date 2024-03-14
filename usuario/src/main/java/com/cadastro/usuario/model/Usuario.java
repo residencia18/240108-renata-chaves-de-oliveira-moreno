6bc9661e843238d2e68750d5aee0fe2556b2f1e6
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-14 11:43:56 -0300

    Add files via upload

---
+++ usuario/src/main/java/com/cadastro/usuario/model/Usuario.java
@@ -0,0 +1,61 @@
+package com.cadastro.usuario.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    // Construtor vazio
+    public Usuario() {
+    }
+
+    // Construtor com todos os campos exceto o ID
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    // Getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
