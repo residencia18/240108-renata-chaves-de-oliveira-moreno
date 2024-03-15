320b410cfd4a9184f20dc6507719ff254c259457
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-15 10:44:15 -0300

    Add files via upload

---
+++ java-avancado/aula-03/tarefas/src/test/java/tarefas/MinhaClasse.java
@@ -0,0 +1,9 @@
+package tarefas;
+
+
+public class MinhaClasse {
+
+    public static int metodoDaMinhaClasse(int i, int j) {
+        return i + j;
+    }
+}
---
+++ java-avancado/aula-03/tarefas/src/test/java/tarefas/MinhaClasseTest.java
@@ -0,0 +1,15 @@
+package tarefas;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MinhaClasseTest {
+
+    @Test
+    public void testMetodoDaMinhaClasse() {
+        
+        assertEquals(4, MinhaClasse.metodoDaMinhaClasse(2, 2));
+    }
+
+  
+}
