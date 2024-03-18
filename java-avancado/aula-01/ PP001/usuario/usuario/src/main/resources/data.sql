fc36547bdeb072e49380e3cda66db426abe74502
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-18 09:28:44 -0300

    Add files via upload

---
+++ java-avancado/aula-01/ PP001/usuario/usuario/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario (nome, email, senha) VALUES ('João', 'joao@example.com', 'senha123');
+INSERT INTO usuario (nome, email, senha) VALUES ('Maria', 'maria@example.com', 'senha456');
+INSERT INTO usuario (nome, email, senha) VALUES ('Renata', 'renatachom.ads@gmail.com', 'senha789');
+
