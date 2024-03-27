1aedd305c897c9b40e151ed7dcdf1bcbbb10d137
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-27 09:18:09 -0300

    Delete java-avancado directory

--- java-avancado/aula-04/PP004/funcionario/funcionario/src/main/java/com/renata/gerenciamento/funcionario/repository/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.renata.gerenciamento.funcionario.repository;
-
-import com.renata.gerenciamento.funcionario.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
