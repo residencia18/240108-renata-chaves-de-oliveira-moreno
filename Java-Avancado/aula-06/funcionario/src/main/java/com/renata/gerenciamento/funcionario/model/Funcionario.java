114be2547f0b9dc9429ba834fc306a4fbed603bd
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-27 08:53:13 -0300

    Add files via upload

---
+++ Java-Avancado/aula-06/funcionario/src/main/java/com/renata/gerenciamento/funcionario/model/Funcionario.java
@@ -0,0 +1,21 @@
+package com.renata.gerenciamento.funcionario.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+    private String cargo;
+  
+
+}
