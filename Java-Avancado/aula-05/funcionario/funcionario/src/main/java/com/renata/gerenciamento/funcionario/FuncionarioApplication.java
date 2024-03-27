9c0be969ca49754f500fb96d1ed65c7216da2b06
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-27 09:15:03 -0300

    Add files via upload

---
+++ Java-Avancado/aula-05/funcionario/funcionario/src/main/java/com/renata/gerenciamento/funcionario/FuncionarioApplication.java
@@ -0,0 +1,23 @@
+package com.renata.gerenciamento.funcionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//@Slf4j
+@SpringBootApplication
+public class FuncionarioApplication {
+	private static Class<FuncionarioApplication> clazz;
+	public static final Logger log = LoggerFactory.getLogger
+	(clazz=FuncionarioApplication.class);
+	
+
+public static void main(String[] args) {
+	SpringApplication.run(FuncionarioApplication.class, args);
+	
+	String msg;
+	log.info(msg="Aplication started...");
+}
+
+}
