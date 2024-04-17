c8c533064507f0440c55f3c89c00fdccd38265cd
renatachom <renatachom.ads@gmail.com>
2024-04-17 10:52:44 -0300

    Corrigindo Projeto

--- Java-Avancado/aula-01/PP001/usuario/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO usuario (nome, email, senha) VALUES ('João', 'joao@example.com', 'senha123');
-INSERT INTO usuario (nome, email, senha) VALUES ('Maria', 'maria@example.com', 'senha456');
-INSERT INTO usuario (nome, email, senha) VALUES ('Renata', 'renatachom.ads@gmail.com', 'senha789');
-
